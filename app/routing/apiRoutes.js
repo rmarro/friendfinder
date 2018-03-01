98f31c21535fbeb792698a890c4d452981212f22
rmarro <rmarro47@gmail.com>
2018-02-28 21:22:01 -0600

    score comparison between new user and one friend is working for post, console.logs the difference

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,6 +13,21 @@ module.exports = function(app) {
     app.post("/api/friends", function(req, res) {
         var newUser = req.body;
         console.log(newUser);
+        userScores = newUser.scores;
+        // loop through all objects in friendList
+        // access the scores key of the object
+        friendScores = friendList[0].scores;
+        // compare each score in that array to the newUser scores array and
+        //add up total of absolute value of differences
+        var differenceArray = [];
+        for (var i = 0; i < friendScores.length; i++) {
+            differenceArray.push(Math.abs(friendScores[i] - userScores[i]))
+        };
+        var differenceTotal = differenceArray.reduce(function (a, b) {
+            return a + b;
+        }, 0);
+        console.log("Difference Score: " + differenceTotal);
+
         friendList.push(newUser);
         res.send("New user added");
     });
@@ -21,9 +36,8 @@ module.exports = function(app) {
 };
 
 
-// extended = true fixes sending arrays to server?????
-
-
+//--------------------------------------
+// trying to save all data to friends.js instead of array here-- does that matter??
 
 // function addfriend() {
 //     var newf = "hello";
@@ -33,6 +47,5 @@ module.exports = function(app) {
 //         console.log("added!")
 //     })
 // };
-
 
 // addfriend();
