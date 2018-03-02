15072fab2fc5e68043435495c5dc3b49a85d88d9
rmarro <rmarro47@gmail.com>
2018-03-01 20:47:33 -0600

    updated html and api route variables

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var htmlRoutes = require(path.join(__dirname, "app/routing/htmlRoutes.js"));
+var apiRoutes = require(path.join(__dirname, "app/routing/apiRoutes.js"));
+
 var app = express();
 var PORT = 3000;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-// Require html and api routes
-require(path.join(__dirname, "app/routing/htmlRoutes.js"))(app);
-require(path.join(__dirname, "app/routing/apiRoutes.js"))(app);
-
+htmlRoutes(app);
+apiRoutes(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
