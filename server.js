0da60e737af39176e241d8d599b3a7c5edbda5e5
rmarro <rmarro47@gmail.com>
2018-03-04 17:38:04 -0600

    still trying to fix heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var htmlRoutes = require(path.join(__dirname, "app/routing/htmlRoutes.js"));
 var apiRoutes = require(path.join(__dirname, "app/routing/apiRoutes.js"));
 
 var app = express();
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
